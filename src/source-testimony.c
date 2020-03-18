e40bfbed2293b648fc431f5bb3b821b546b4e9d3
Mark <alnyan@airmail.cc>
2020-03-18 14:40:02 +0200

    Add testimony read/decode support

--- src/source-testimony.c
+++ src/source-testimony.c
@@ -6,15 +6,19 @@
 
 #include "source-testimony.h"
 
-#include <stdio.h>
+#include <testimony.h>
 
 typedef struct TestimonyThreadVars_ {
     ThreadVars *tv;
+    TmSlot *slot;
+    testimony_iter iter;
+    testimony t;
     int running;
 } TestimonyThreadVars;
 
 static TmEcode ReceiveTestimonyLoop(ThreadVars *tv, void *data, void *slot);
 static TmEcode ReceiveTestimonyThreadInit(ThreadVars *tv, const void *initdata, void **data);
+static TmEcode ReceiveTestimonyThreadDeinit(ThreadVars *tv, void *data);
 static TmEcode ReceiveTestimonyBreakLoop(ThreadVars *tv, void *data);
 static void ReceiveTestimonyThreadExitStats(ThreadVars *tv, void *data);
 
@@ -24,12 +28,12 @@ static TmEcode DecodeTestimony(ThreadVars *, Packet *, void *);
 
 void TmModuleReceiveTestimonyRegister (void)
 {
-    printf("%s\n", __func__);
     tmm_modules[TMM_RECEIVETESTIMONY].name = "ReceiveTestimony";
     tmm_modules[TMM_RECEIVETESTIMONY].ThreadInit = ReceiveTestimonyThreadInit;
     tmm_modules[TMM_RECEIVETESTIMONY].PktAcqLoop = ReceiveTestimonyLoop;
     tmm_modules[TMM_RECEIVETESTIMONY].PktAcqBreakLoop = ReceiveTestimonyBreakLoop;
     tmm_modules[TMM_RECEIVETESTIMONY].ThreadExitPrintStats = ReceiveTestimonyThreadExitStats;
+    tmm_modules[TMM_RECEIVETESTIMONY].ThreadDeinit = ReceiveTestimonyThreadDeinit;
     tmm_modules[TMM_RECEIVETESTIMONY].cap_flags = 0; //SC_CAP_NET_RAW;
     tmm_modules[TMM_RECEIVETESTIMONY].flags = TM_FLAG_RECEIVE_TM;
 }
@@ -43,14 +47,78 @@ void TmModuleDecodeTestimonyRegister (void)
     tmm_modules[TMM_DECODETESTIMONY].flags = TM_FLAG_DECODE_TM;
 }
 
+static void AddTestimonyPacket(TestimonyThreadVars *ttv, ThreadVars *tv, const struct tpacket3_hdr *tp)
+{
+    const uint8_t *packet_data;
+    size_t packet_length;
+
+    SCEnter();
+
+    packet_data = testimony_packet_data(tp);
+    if (unlikely(packet_data == NULL)) {
+        SCReturn;
+    }
+    packet_length = tp->tp_snaplen;
+
+    Packet *p = PacketGetFromQueueOrAlloc();
+    if (unlikely(p == NULL)) {
+        SCReturn;
+    }
+
+    PKT_SET_SRC(p, PKT_SRC_WIRE);
+    p->ts.tv_sec = tp->tp_sec;
+    p->ts.tv_usec = tp->tp_nsec / 1000;
+    p->datalink = LINKTYPE_ETHERNET;
+
+    if (unlikely(PacketCopyData(p, packet_data, packet_length))) {
+        TmqhOutputPacketpool(tv, p);
+        SCReturn;
+    }
+
+    if (TmThreadsSlotProcessPkt(tv, ttv->slot, p) != TM_ECODE_OK) {
+        ttv->running = 0;
+    }
+}
+
 static TmEcode ReceiveTestimonyLoop(ThreadVars *tv, void *data, void *slot)
 {
+    int res;
+    const struct tpacket_block_desc* block;
+    const struct tpacket3_hdr* packet;
+
     SCEnter();
-    printf("%s\n", __func__);
     TestimonyThreadVars *ttv = (TestimonyThreadVars *) data;
 
+    TmSlot *s = (TmSlot *)slot;
+    ttv->slot = s->slot_next;
+
+    testimony_iter_init(&ttv->iter);
+
     while (ttv->running) {
-        usleep(100000);
+        res = testimony_get_block(ttv->t, 100, &block);
+        if (res == 0 && !block) {
+            // Timed out
+            continue;
+        }
+        if (res < 0) {
+            SCLogError(SC_ERR_TESTIMONY_GET_BLOCK, "testimony_get_block(): %s, %s",
+                    testimony_error(ttv->t),
+                    strerror(-res));
+            SCReturnInt(TM_ECODE_FAILED);
+        }
+
+        testimony_iter_reset(ttv->iter, block);
+        while ((packet = testimony_iter_next(ttv->iter)) != NULL) {
+            AddTestimonyPacket(ttv, tv, packet);
+        }
+
+        res = testimony_return_block(ttv->t, block);
+        if (res < 0) {
+            SCLogError(SC_ERR_TESTIMONY_GET_BLOCK, "testimony_return_block(): %s, %s",
+                    testimony_error(ttv->t),
+                    strerror(-res));
+            SCReturnInt(TM_ECODE_FAILED);
+        }
     }
 
     SCReturnInt(TM_ECODE_OK);
@@ -58,10 +126,32 @@ static TmEcode ReceiveTestimonyLoop(ThreadVars *tv, void *data, void *slot)
 
 static TmEcode ReceiveTestimonyThreadInit(ThreadVars *tv, const void *initdata, void **data)
 {
+    // TODO: pass this via config
+    const char *socket_path = "testimony.sock";
+    int res;
+
     SCEnter();
-    printf("%s\n", __func__);
+
     TestimonyThreadVars *ttv = SCCalloc(1, sizeof(TestimonyThreadVars));
     if (unlikely(ttv == NULL)) {
+        SCReturnInt(TM_ECODE_FAILED);
+    }
+
+    SCLogInfo("Socket path: %s", socket_path);
+
+    res = testimony_connect(&ttv->t, socket_path);
+    if (res < 0) {
+        SCLogError(SC_ERR_TESTIMONY_CREATE, "testimony_connect(): %s", strerror(-res));
+        //perror("testimony_connect()");
+        SCReturnInt(TM_ECODE_FAILED);
+    }
+
+    res = testimony_init(ttv->t);
+    if (res < 0) {
+        SCLogError(SC_ERR_TESTIMONY_CREATE, "testimony_init(): %s, %s",
+                testimony_error(ttv->t),
+                strerror(-res));
+        testimony_close(ttv->t);
         SCReturnInt(TM_ECODE_FAILED);
     }
 
@@ -72,38 +162,58 @@ static TmEcode ReceiveTestimonyThreadInit(ThreadVars *tv, const void *initdata, 
     SCReturnInt(TM_ECODE_OK);
 }
 
+static TmEcode ReceiveTestimonyThreadDeinit(ThreadVars *tv, void *data) {
+    SCEnter();
+
+    TestimonyThreadVars *ttv = (TestimonyThreadVars *) data;
+    testimony_close(ttv->t);
+
+    SCReturnInt(TM_ECODE_OK);
+}
+
 static TmEcode ReceiveTestimonyBreakLoop(ThreadVars *tv, void *data)
 {
     SCEnter();
-    printf("%s\n", __func__);
     TestimonyThreadVars *ttv = (TestimonyThreadVars *)data;
     ttv->running = 0;
-    //if (ptv->pcap_handle == NULL) {
-    //    SCReturnInt(TM_ECODE_FAILED);
-    //}
-    //pcap_breakloop(ptv->pcap_handle);
     SCReturnInt(TM_ECODE_OK);
 }
 
 static void ReceiveTestimonyThreadExitStats(ThreadVars *tv, void *data)
 {
     SCEnter();
+    SCReturn;
 }
 
 static TmEcode DecodeTestimony(ThreadVars *tv, Packet *p, void *data)
 {
     SCEnter();
+    DecodeThreadVars *dtv = (DecodeThreadVars *)data;
+
+    // All packets are assumed to be ethernet when using testimony
+    DecodeEthernet(tv, dtv, p, GET_PKT_DATA(p), GET_PKT_LEN(p));
+
     SCReturnInt(TM_ECODE_OK);
 }
 
 static TmEcode DecodeTestimonyThreadInit(ThreadVars *tv, const void *initdata, void **data)
 {
-
     SCEnter();
+
+    DecodeThreadVars *dtv = DecodeThreadVarsAlloc(tv);
+    if (unlikely(dtv == NULL)) {
+        SCReturnInt(TM_ECODE_FAILED);
+    }
+
+    DecodeRegisterPerfCounters(dtv, tv);
+
+    *data = (void *)dtv;
+
     SCReturnInt(TM_ECODE_OK);
 }
 
 static TmEcode DecodeTestimonyThreadDeinit(ThreadVars *tv, void *data)
 {
+    SCEnter();
     SCReturnInt(TM_ECODE_OK);
 }
